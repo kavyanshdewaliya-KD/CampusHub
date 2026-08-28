311453b32a72b5536fffe9766b26b8c721b74d91
kavyanshdewaliya-KD <kavyanshdewaliya8@gmail.com>
2026-08-28 15:55:08 +0530

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBURkftN68dMpRFrnWBdca0qs1QmpQlzQ0",
+  projectId: "ietcse-bb0c9",
+  messagingSenderId: "39297300181",
+  appId: "1:39297300181:web:08b6d3e5bae40b9c19145c"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title || "Campusmate Notice";
+  const notificationOptions = {
+    body: payload.notification.body || "New update posted for IET DAVV CSE-B",
+    icon: "/icon.png"
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
